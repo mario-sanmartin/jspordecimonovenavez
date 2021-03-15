0446ca89a4289e7b19bb10dedcdf0e09cd638987
mario-sanmartin <sanmartin.mario@outlook.com>
2021-03-15 02:08:53 -0300

    Js Windows

---
+++ 01-window/main.js
@@ -0,0 +1,10 @@
+let youtubeURL = "https://youtube.com";
+// let ventana = window.open(youtubeURL);
+// ventana.closed //pregunta si esta cerrada
+// ventana.close();
+// ventana.stop() //detiene la ventana
+
+// print() //imprime la pantalla
+// prompt("dame un beso") //pide un string 
+let respuesta = confirm("¿Estas seguro que deseas salir?")
+console.log(respuesta);
